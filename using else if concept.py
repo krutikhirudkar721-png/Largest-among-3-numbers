e760f478d57f7984c8e0682ca0bd65a93dc1819c
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-06-05 22:43:17 +0530

    Update and rename find largest among.py to using else if concept.py

--- using else if concept.py
+++ using else if concept.py
@@ -12,18 +12,8 @@
     else:
       print(f"The largest number is: {n3}")
 
-
-# #Using while loop print Hello Students
-#     n = int(input("Enter the number of times to print: "))
-#     print("Using while loop:")
-#     i=1
-#     while i<=n:
-# 	    print("Hello Students")
-# 	    i+=1
-
-
 #Using for loop Hello Students
-	print("Using for loop:")
-    for i in range( n    ):
-        print("Hello Students")
+# 	print("Using for loop:")
+#     for i in range( n    ):
+#         print("Hello Students")
     
