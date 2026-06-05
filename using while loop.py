d690e622eef559fb7e4da34878a3a52bf1b44fcd
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-06-05 22:42:14 +0530

    Create using while loop.py

---
+++ using while loop.py
@@ -0,0 +1,8 @@
+# Using while loop print Hello Students
+
+    n = int(input("Enter the number of times to print: "))
+    print("Using while loop:")
+    i=1
+    while i<=n:
+	    print("Hello Students")
+	    i+=1
