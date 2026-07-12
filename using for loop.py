a931db05900ecce312353f08758ad98364b49c6a
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-07-12 20:47:28 +0530

    Update using for loop.py

--- using for loop.py
+++ using for loop.py
@@ -3,16 +3,11 @@
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
 n3 = int(input("Enter third number: "))
-
 # Put numbers in a list
-
 numbers = [n1, n2, n3]
-
 # Loop to find the largest
-
 largest = n1
 for num in numbers:
     if num > largest:
         largest = num
-
 print(f"The largest number is: {largest}")
