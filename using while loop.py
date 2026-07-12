9323d1552d09aae020229c90b533d05aaac944b7
Krutik Hirudkar <krutikhirudkar721@gmail.com>
2026-07-12 20:47:02 +0530

    Update using while loop.py

--- using while loop.py
+++ using while loop.py
@@ -1,18 +1,12 @@
 # Using while loop print Hello Students
-
 n1 = int(input("Enter first number: "))
 n2 = int(input("Enter second number: "))
 n3 = int(input("Enter third number: "))
-
 numbers = [n1, n2, n3]
-
 # Variables for while loop
-
 largest = n1
 index = 0
-
 # Loop running until index reaches length of list (3)
-
 while index < len(numbers):
     if numbers[index] > largest:
         largest = numbers[index]
